4480fd8b011036a23de86a5726be464f319c0935
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-14 14:49:11 +0530

    sign up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
+import { SiginupComponent } from './siginup/siginup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigninComponent
+    SigninComponent,
+    SiginupComponent
   ],
   imports: [
     BrowserModule,
